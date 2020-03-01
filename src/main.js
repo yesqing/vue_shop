5dc20af3f4d8b35437f568c908196e20b87bf898
yesqing <1429189602@qq.com>
2020-03-01 16:53:52 +0800

    完成商品功能的开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import ZTreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -17,7 +24,19 @@ axios.interceptors.request.use(config => {
 Vue.config.productionTip = false
 
 Vue.component('tree-table', ZTreeTable)
+// 将富文本编辑器全局注册
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const year = dt.getFullYear()
+  const mouth = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const day = (dt.getDay() + '').padStart(2, '0')
 
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${year}-${mouth}-${day} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
